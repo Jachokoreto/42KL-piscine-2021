907d58d650cc8f47e5600da658ac92f0a584e13d
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2021-05-25 11:06:44 +0800

    c01 commit

---
+++ c01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,30 @@
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = *temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	min;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		min = i;
+		while (j < size)
+		{
+			if (tab[j] < tab[min])
+				min = j;
+			j++;
+		}
+		ft_swap(&tab[min], &tab[i]);
+		i++;
+	}
+}
